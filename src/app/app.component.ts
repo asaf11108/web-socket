b46548f3b9fb2f04c806eb2285a2247d11dcf064
asaf11108 <asaf11108@gmail.com>
2023-08-15 16:00:34 +0300

    initWebSocketServer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import  { WebSocketServer } from 'ws';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'web-socket';
+
+  ngOnInit(): void {
+    this.initWebSocketServer()
+  }
+
+  private initWebSocketServer() {
+    const wss = new WebSocketServer({ port: 8080 });
+
+    wss.on('error', console.error);
+
+    wss.on('open', function open(ws) {
+      ws.send('something');
+    });
+
+    wss.on('connection', function connection(ws) {
+      ws.on('message', function incoming(message) {
+        ws.send('test');
+      });
+
+      ws.send('hello from the server!');
+    });
+  }
 }
