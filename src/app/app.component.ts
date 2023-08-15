c92feb19d9721110fa7728431eb28ff1069d0298
asaf11108 <asaf11108@gmail.com>
2023-08-15 16:21:40 +0300

    ws

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import  { WebSocketServer } from 'ws';
+import { Component, OnDestroy } from '@angular/core';
+import { WebSocketServiceService } from './web-socket-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnDestroy {
   title = 'web-socket';
 
-  ngOnInit(): void {
-    this.initWebSocketServer()
+  constructor(public webSocketService: WebSocketServiceService) {
+    this.webSocketService.connect();
   }
 
-  private initWebSocketServer() {
-    const wss = new WebSocketServer({ port: 8080 });
+  sendMessage(message: string) {
+    this.webSocketService.sendMessage(message);
+  }
 
-    wss.on('error', console.error);
-
-    wss.on('open', function open(ws) {
-      ws.send('something');
-    });
-
-    wss.on('connection', function connection(ws) {
-      ws.on('message', function incoming(message) {
-        ws.send('test');
-      });
-
-      ws.send('hello from the server!');
-    });
+  ngOnDestroy() {
+    this.webSocketService.close();
   }
 }
